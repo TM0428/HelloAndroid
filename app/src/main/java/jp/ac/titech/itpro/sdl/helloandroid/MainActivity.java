237ec1bc5f1071e8588b2898c04ddcb71d6c72a6
t.m <takashi.orion.999.428@gmail.com>
2022-08-06 16:32:00 +0900

    data bindingに対応

--- app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.java
@@ -7,22 +7,34 @@ import android.view.View;
 import android.widget.EdgeEffect;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import jp.ac.titech.itpro.sdl.helloandroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private TextView outputView;
     private EditText inputName;
+    private ActivityMainBinding mBinding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        outputView = findViewById(R.id.output_view);
-        inputName = findViewById(R.id.input_name);
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.btReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"This is called by Script",Toast.LENGTH_SHORT).show();
+                mBinding.inputName.setText("");
+                mBinding.tvGreet.setText(R.string.enter_name);
+            }
+        });
     }
 
     public void greet(View V){
-        String name = inputName.getText().toString().trim();
+        String name = mBinding.inputName.getText().toString().trim();
+        Toast.makeText(getApplicationContext(),"This is called by layout",Toast.LENGTH_SHORT).show();
         if(name.length() > 0){
-            outputView.setText("Hello " + name + "!\nNice to see you!");
+            mBinding.tvGreet.setText("Hello " + name + "!\nNice to see you!");
         }
     }
 }
